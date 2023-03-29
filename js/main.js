9ad7be15e8ae896203758628bd336dcfece4f278
arturgomesc <arturgomesc@hotmail.com>
2023-03-29 17:26:52 -0300

    manipuladados e foreach

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
-const robotron = document.querySelector('#robotron');
+// addEventListener: manipulador de evento, adiciona ações a um ID, por exemplo. estrutura: target.addEventListener(event, function, useCapture)
+// parseInt: transforma um valor string em inteiro, deixa de se comportar com concatenação e passa a ser tratado como número.
+// querySelector: seleciona um elemento do HTML
+// arrow functions: representadas por uma "flecha" ()=> {} , são utilizadas pra representar funções únicas que não serão reutilizadas
+// forEach:
+//target, textContent, parentNode
 
-robotron.addEventListener("click", (evento) => {
-    console.log(evento);
-});
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
 
-function dizOi(nome) {
-    console.log("oi " + nome);
-    console.log('Bem vindo ao Robotron 2000.');
+const controle = document.querySelectorAll(".controle-ajuste")
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode);
+    })
+})
+
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector('.controle-contador');
+
+    if (operacao === "-") {
+        peca.value = parseInt(peca.value) - 1;
+    } else {
+        peca.value = parseInt(peca.value) + 1;
+    }
 }
-
-dizOi("Artur") 
